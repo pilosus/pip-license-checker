28e2fd1399e94a40f91056f68dc515bd1d1e366f
Vitaly Samigullin <vrs@pilosus.org>
2020-11-21 21:39:19 +0300

    MVP to check a single package

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject pip-license-checker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Check Python PyPI package license"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.1"]
+                 [clj-http "3.11.0"]
+                 [cheshire "5.10.0"]]
   :main ^:skip-aot pip-license-checker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/pip_license_checker/core.clj
+++ src/pip_license_checker/core.clj
@@ -1,7 +1,38 @@
 (ns pip-license-checker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as http])
+  (:require [cheshire.core :as json])
+  (:require [clojure.walk :as walk]))
+
+(defn 
+  client-get-or-nil
+  [url]
+  (try
+    (http/get url)
+    (catch Exception e nil)))
+
+
+(defn 
+  get-package-response
+  [name version]
+  (let [base-url "https://pypi.org/pypi/"
+        url (str base-url name "/" version "/json" )]
+    (client-get-or-nil url)))
+
+
+(defn 
+  get-license
+  [name version]
+  (let [data (get-package-response name version)]
+    (if (some? data) 
+      ;; TODO refactor to a separate function, make fault tolerant
+      (:license (:info (walk/keywordize-keys (json/parse-string (:body data)))))
+      ("Error"))))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (= (count args) 2) 
+    (println (get-license (first args) (second args)))  
+    ;; TODO move to separate usage function
+    (println "Usage: pip_license_checker name version")))
