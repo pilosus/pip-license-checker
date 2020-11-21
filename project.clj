12ad25d92289c7ba01b92ca92bc59d5a431c577d
Vitaly Samigullin <vrs@pilosus.org>
2020-11-21 21:42:34 +0300

    Fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pip-license-checker "0.1.0-SNAPSHOT"
   :description "Check Python PyPI package license"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/pilosus/pip-license-checker"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
