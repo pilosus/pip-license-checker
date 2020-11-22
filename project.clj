1bc18872bda691c6bff18b0327e8316c1c6b9eaa
Vitaly Samigullin <vrs@pilosus.org>
2020-11-22 14:07:39 +0300

    Fix documentation and license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject pip-license-checker "0.1.0-SNAPSHOT"
   :description "Check Python PyPI package license"
   :url "https://github.com/pilosus/pip-license-checker"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License (MIT)"
+            :url "https://github.com/pilosus/pip-license-checker/blob/main/LICENSE"}
   :dependencies [
                  [org.clojure/clojure "1.10.1"]
                  [clj-http "3.11.0"]
